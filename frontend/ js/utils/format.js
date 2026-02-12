d9adda8a3e6d140d63b2df679e6ca32d405cbefb
samarth088 <skshah987654@gmail.com>
2026-02-13 01:51:50 +0530

    Update format.js

--- frontend/ js/utils/format.js
+++ frontend/ js/utils/format.js
@@ -1,1 +1,13 @@
+// Shorten long message preview
+export function shortenText(text, length = 30) {
+  if (!text) return "";
+  return text.length > length
+    ? text.substring(0, length) + "..."
+    : text;
+}
 
+// Capitalize first letter
+export function capitalize(text) {
+  if (!text) return "";
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
