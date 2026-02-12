48856fa81d64603f0440c05752a354522cf21851
samarth088 <skshah987654@gmail.com>
2026-02-13 01:50:32 +0530

    Update modal.js

--- frontend/ js/ui/modal.js
+++ frontend/ js/ui/modal.js
@@ -1,1 +1,9 @@
+export function openModal(id) {
+  const modal = document.getElementById(id);
+  if (modal) modal.classList.remove("hidden");
+}
 
+export function closeModal(id) {
+  const modal = document.getElementById(id);
+  if (modal) modal.classList.add("hidden");
+}
