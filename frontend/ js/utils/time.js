a86fba912d8c59d6ed20ce138d96d9eaa2f9b8f5
samarth088 <skshah987654@gmail.com>
2026-02-13 01:51:23 +0530

    Update time.js

--- frontend/ js/utils/time.js
+++ frontend/ js/utils/time.js
@@ -1,1 +1,14 @@
+// Format message time (HH:MM)
+export function formatTime(date) {
+  const d = new Date(date);
+  return d.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+}
 
+// Format last seen
+export function formatLastSeen(date) {
+  const d = new Date(date);
+  return "Last seen " + d.toLocaleString();
+}
