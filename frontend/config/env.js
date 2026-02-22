e0ecfa1d61cf0af4c3fe867be66ead4294cda6fb
samarth088 <skshah987654@gmail.com>
2026-02-23 00:58:03 +0530

    Update env.js

--- frontend/config/env.js
+++ frontend/config/env.js
@@ -1,5 +1,5 @@
 window.ENV = {
-  API_URL: 'https://vibechat-992.render.com/api',
-  WS_URL:  'https://vibechat-992.render.com',
+  API_URL: 'https://vibechat-992r.onrender.com/api',
+  WS_URL:  'https://vibechat-992r.onrender.com',
   DEV_MODE: false
 };
