f4fdb6147f7e600e7a5be3a3662a915f487d9d7b
the-sitaram <yraj38377@gmail.com>
2026-02-28 00:11:30 +0530

    Update otp.js

--- backend/utils/otp.js
+++ backend/utils/otp.js
@@ -1,7 +1,26 @@
-const crypto = require("crypto");
+// utils/otp.js
+const crypto = require('crypto');
 
-const generateOTP = () => {
-  return crypto.randomInt(100000, 999999).toString();
-};
+/**
+ * Generate a 6-digit OTP
+ */
+function generateOtp() {
+  // Cryptographically secure random 6-digit number
+  return String(crypto.randomInt(100000, 999999));
+}
 
-module.exports = { generateOTP };
+/**
+ * Get OTP expiry time (10 minutes from now)
+ */
+function getOtpExpiry(minutes = 10) {
+  return new Date(Date.now() + minutes * 60 * 1000);
+}
+
+/**
+ * Check if OTP is expired
+ */
+function isOtpExpired(expiresAt) {
+  return new Date() > new Date(expiresAt);
+}
+
+module.exports = { generateOtp, getOtpExpiry, isOtpExpired };
