5dad0d721dde4be05805ad89377c87147705710f
the-sitaram <yraj38377@gmail.com>
2026-03-02 00:24:26 +0530

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllUsers,
-  getProfile,
-  updateProfile,
-  searchUser,
-} = require("../controllers/user.controller");
-
+const { getAllUsers, getProfile, updateProfile, searchUser } = require("../controllers/user.controller");
 const { protect } = require("../middleware/auth.middleware");
 
-// All routes protected
 router.use(protect);
 
-// GET /users/search?uid=tanuu   ← SEARCH by uid or username
-router.get("/search", searchUser);
-
-// GET /users/me
+router.get("/search", searchUser);  // ← pehle
 router.get("/me", getProfile);
-
-// PUT /users/me
 router.put("/me", updateProfile);
-
-// GET /users/all
 router.get("/all", getAllUsers);
 
 module.exports = router;
